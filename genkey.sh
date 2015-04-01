34fa7a5355718e7c7978f1764abe53292f7c43b9
TYPCN <admin@typcn.com>
2015-04-01 22:25:30 +0800

    add tips for first play

--- genkey.sh
+++ genkey.sh
@@ -1,3 +1,3 @@
 rm -rf bilibili/APIKey.h;
 echo "NSString *APIKey = @\"\";" >> bilibili/APIKey.h;
-echo "NSString *APIKey = @\"\";" >> bilibili/APIKey.h;
+echo "NSString *APISecret = @\"\";" >> bilibili/APIKey.h;
