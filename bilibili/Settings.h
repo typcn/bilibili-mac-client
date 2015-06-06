20229e933bf476e54769a5efe9722e9d40846cff
TYPCN <admin@typcn.com>
2015-06-06 14:50:17 +0800

    add "MP4 format" option

--- bilibili/Settings.h
+++ bilibili/Settings.h
@@ -17,5 +17,7 @@
 @property (weak) IBOutlet NSComboBox *FakeIP;
 @property (weak) IBOutlet NSTextField *fontsize;
 @property (weak) IBOutlet NSButton *disablebottomComment;
+@property (weak) IBOutlet NSButton *playMP4;
+@property (weak) IBOutlet NSButton *DownloadMP4;
 
 @end
