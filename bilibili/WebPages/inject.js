a05a911ceded036b83aa5f4bb68dd9aa22966ee3
TYPCN <admin@typcn.com>
2015-07-31 20:21:37 +0800

    fix live and event page

--- bilibili/WebPages/inject.js
+++ bilibili/WebPages/inject.js
@@ -12,33 +12,39 @@ function applyUI(){
         
     }
     if(window.location.href.indexOf("av") > 1 || window.location.href.indexOf("topic") > 1 || window.location.href.indexOf("live") > 1){
-        var fv=$("param[name='flashvars']").val();
-        if(!fv){
-            fv=$('#bofqiiframe').attr('src');
-        }
-        if(!fv){
-            fv=$('.player').attr('src');
-        }
-        if(!fv){
-            fv=$('.player-wrapper embed').attr('flashvars');
-        }
-        if(!fv){
-            if(!window.ROOMID){
-                clearInterval(window.i);
-                return;
+        console.log("getting cid");
+        try{
+            var fv=$("param[name='flashvars']").val();
+            if(!fv){
+                fv=$('#bofqiiframe').attr('src');
             }
-            fv = 'cid=' + ROOMID;
+            if(!fv){
+                fv=$('.player').attr('src');
+            }
+            if(!fv){
+                fv=$('.player-wrapper embed').attr('flashvars');
+            }
+            if(!fv){
+                fv = 'cid=' + $("#modVideoCid").text() + '&';
+            }
+            if(window.ROOMID){
+                fv = 'cid=' + window.ROOMID + '&';
+            }
+        }catch(e){
+            console.log(e);
         }
+        console.log("fv:" + fv);
         var re = /cid=(\d+)&/;
         var m = re.exec(fv);
         TYPCN_PLAYER_CID = m[1];
-        
+        console.log("cid got");
         if(!$('.i_face').attr('src')){
             $('.login').css('width',200).children('a').html('点击登录客户端以便发送弹幕');
         }
         
         if(TYPCN_PLAYER_CID){
             if(window.location.origin == 'http://live.bilibili.com'){
+                console.log("inject live page");
                 if(window.ROOMID > 0){
                     if(typeof LIVEPLAY == "undefined"){
                         window.external.playVideoByCID(ROOMID.toString());
@@ -46,16 +52,18 @@ function applyUI(){
                     }
                 }
             }else if(window.location.href.indexOf("topic") > 1){
+                console.log("inject topic page");
                 $('embed').parent().html('<div class="TYPCN_PLAYER_INJECT_PAGE"><div class="player-placeholder"><div class="btn-wrapper n2"><div class="player-placeholder-head">请选择操作</div><div class="src-btn"><a href="javascript:window.external.playVideoByCID(TYPCN_PLAYER_CID)">播放</a></div><div class="src-btn"><a href="javascript:window.external.downloadVideoByCID(TYPCN_PLAYER_CID)">下载</a></div></div></div></div>');
             }else{
+                console.log("inject player page");
                 $('#bofqi').html('<div class="TYPCN_PLAYER_INJECT_PAGE"><div class="player-placeholder"><div class="btn-wrapper n2"><div class="player-placeholder-head">请选择操作</div><div class="src-btn"><a href="javascript:window.external.playVideoByCID(TYPCN_PLAYER_CID)">播放</a></div><div class="src-btn"><a href="javascript:window.external.downloadVideoByCID(TYPCN_PLAYER_CID)">下载</a></div></div></div></div>');
             }
-            
+            clearInterval(window.i);
         }else{
             
         }
         console.log("inject success");
-        clearInterval(window.i);
+        
     }
 }
 if(!window.isFirstPlay){
@@ -63,7 +71,7 @@ if(!window.isFirstPlay){
     window.i = setInterval(waitForReady,500);
     console.log("start inject");
     function waitForReady(){
-        if(window.LoadTimes > 15){
+        if(window.LoadTimes > 10){
             clearInterval(window.i);
             return;
         }
