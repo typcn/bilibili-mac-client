0fca9b5f8b96a4dfab939084202a2c3391f1aba8
Tong Zhang <tongzhang101x@gmail.com>
2015-09-03 23:22:17 +0800

    Use precise seeks whenever possible.

--- bilibili/PlayerView/PlayerView.mm
+++ bilibili/PlayerView/PlayerView.mm
@@ -375,7 +375,7 @@ GetInfo:NSDictionary *VideoInfoJson = [self getVideoInfo:firstVideo];
     check_error(mpv_set_option_string(mpv, "script-opts", "osc-layout=bottombar,osc-seekbarstyle=bar"));
     check_error(mpv_set_option_string(mpv, "user-agent", [@"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0.2) Gecko/20100101 Firefox/6.0.2 Fengfan/1.0" cStringUsingEncoding:NSUTF8StringEncoding]));
     check_error(mpv_set_option_string(mpv, "framedrop", "vo"));
-    
+    check_error(mpv_set_option_string(mpv, "hr-seek", "yes"));
     
     int disableKeepAspect = [self getSettings:@"disableKeepAspect"];
     if(disableKeepAspect == 1){
