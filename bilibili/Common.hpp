430ff9ee480a45a6d63bf01e2dcde3df699c62fb
TYPCN <admin@typcn.com>
2015-09-04 14:05:04 +0800

    move global vars

---
+++ bilibili/Common.hpp
@@ -0,0 +1,33 @@
+//
+//  Common.h
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/4.
+//  Copyright (c) 2015 TYPCN. All rights reserved.
+//
+
+#ifndef bilibili_Common_h
+#define bilibili_Common_h
+
+#import "Browser.h"
+
+#ifdef __cplusplus
+#import "downloadWrapper.h"
+extern Downloader* DL;
+#endif
+
+extern Browser* browser;
+extern NSString *vUrl;
+extern NSString *vCID;
+extern NSString *vTitle;
+extern NSString *userAgent;
+extern NSWindow *currWindow;
+extern NSMutableArray *downloaderObjects;
+
+extern NSLock *dList;
+extern BOOL parsing;
+
+extern NSString *cmFile;
+extern NSString *subFile;
+
+#endif
---
+++ bilibili/MainView/Browser.h
@@ -0,0 +1,14 @@
+//
+//  Browser.h
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/3.
+//  Copyright (c) 2015 TYPCN. All rights reserved.
+//
+
+#import <ChromiumTabs/ChromiumTabs.h>
+#import "AppDelegate.h"
+
+@interface Browser : CTBrowser
+
+@end
