899ae6e83c3d91d24e8330e72a2e15ed0c591aaa
TYPCN <admin@typcn.com>
2015-09-04 19:28:28 +0800

    Add toolbar subclass

---
+++ bilibili/MainView/ToolBar.h
@@ -0,0 +1,13 @@
+//
+//  ToolBar.h
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/4.
+//  Copyright (c) 2015 TYPCN. All rights reserved.
+//
+
+#import <ChromiumTabs/ChromiumTabs.h>
+
+@interface BLToolBar : CTToolbarController
+
+@end
---
+++ bilibili/MainView/ToolBar.m
@@ -0,0 +1,32 @@
+//
+//  ToolBar.m
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/4.
+//  Copyright (c) 2015 TYPCN. All rights reserved.
+//
+
+#import "ToolBar.h"
+
+@interface BLToolBar ()
+@property (weak) IBOutlet NSTextField *URLInputField;
+
+@end
+
+@implementation BLToolBar
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (IBAction)goHome:(id)sender {
+}
+- (IBAction)Refresh:(id)sender {
+}
+- (IBAction)forward:(id)sender {
+}
+- (IBAction)backward:(id)sender {
+}
+- (IBAction)menu:(id)sender {
+}
+
+@end
