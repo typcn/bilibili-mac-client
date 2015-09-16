a2a9414c50127f48ece55664c6e37c61e72d77fe
TYPCN <admin@typcn.com>
2015-09-16 20:16:08 +0800

    Add AirPlay view , rename some files

---
+++ bilibili/PlayerView/AirPlayView.h
@@ -0,0 +1,13 @@
+//
+//  AirPlayView.h
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/16.
+//  Copyright © 2015 TYPCN. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AirPlayView : NSViewController
+
+@end
---
+++ bilibili/PlayerView/AirPlayView.m
@@ -0,0 +1,22 @@
+//
+//  AirPlayView.m
+//  bilibili
+//
+//  Created by TYPCN on 2015/9/16.
+//  Copyright © 2015 TYPCN. All rights reserved.
+//
+
+#import "AirPlayView.h"
+
+@interface AirPlayView ()
+
+@end
+
+@implementation AirPlayView
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
