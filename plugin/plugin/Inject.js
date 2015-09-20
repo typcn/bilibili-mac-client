e5d2e3bd3d87038005e72889f985a129de8e7344
TYPCN <admin@typcn.com>
2015-09-20 21:57:44 +0800

    Add plugin example

---
+++ plugin/plugin/Inject.js
@@ -0,0 +1,16 @@
+
+window.sendToPlugin = function(data){
+    var request = new XMLHttpRequest();
+    request.open('POST', 'http://localhost:23330/pluginCall', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.send(data);
+}
+
+
+document.body.innerHTML = '<a id="test" href="#">Click to test</a>';
+
+document.getElementById('test').addEventListener('click',testclick);
+
+function testclick(){
+    sendToPlugin({ action:'Example-ShowExamplePanel', data:'123' });
+}
