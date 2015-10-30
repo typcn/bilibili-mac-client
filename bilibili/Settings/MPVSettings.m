d61439ca28d077c015ec9ce5f4416985d112c140
TYPCN <admin@typcn.com>
2015-10-31 07:11:49 +0800

    Add mpv config settings

---
+++ bilibili/Settings/MPVSettings.m
@@ -0,0 +1,39 @@
+//
+//  MPVSettings.m
+//  bilibili
+//
+//  Created by TYPCN on 2015/10/31.
+//  Copyright © 2015 TYPCN. All rights reserved.
+//
+
+#import "MPVSettings.h"
+
+@interface MPVSettings ()
+@property (unsafe_unretained) IBOutlet NSTextView *textView;
+
+@end
+
+@implementation MPVSettings
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString *settings = [[NSUserDefaults standardUserDefaults] objectForKey:@"mpvSettings"];
+    if([settings length] > 1){
+        [self.textView setString:settings];
+    }
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(windowWillClose:) name:NSWindowWillCloseNotification object:self.view.window];
+}
+
+- (void)windowWillClose:(NSNotification *)notification
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+    if(![[self.textView string] length]){
+        return;
+    }
+    
+    [[NSUserDefaults standardUserDefaults] setObject:[self.textView string] forKey:@"mpvSettings"];
+}
+
+@end
