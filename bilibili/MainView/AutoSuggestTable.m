f145fa73d33756abc27355282254117cc929e7f8
TYPCN <admin@typcn.com>
2016-01-26 04:35:57 +0800

    Add address bar online word suggest

--- bilibili/MainView/AutoSuggestTable.m
+++ bilibili/MainView/AutoSuggestTable.m
@@ -16,6 +16,8 @@ extern NSString *sharedURLFieldString;
     NSImage *icon_webpage;
     NSImage *icon_search;
     NSArray *item_cache;
+    NSString *online_cache_key;
+    NSMutableArray *online_cache;
 }
 
 - (id)init{
@@ -34,18 +36,29 @@ extern NSString *sharedURLFieldString;
 
 
 - (BOOL)tableView:(NSTableView *)tableView shouldSelectRow:(NSInteger)rowIndex {
-    if([item_cache count] > rowIndex){
+    int LocalCacheCount = (int)[item_cache count];
+    int OnlineCacheIndex = (int)rowIndex - LocalCacheCount;
+    if(LocalCacheCount > rowIndex){
         NSString *url = [item_cache objectAtIndex:rowIndex];
         WebTabView *tc = (WebTabView *)[browser activeTabContents];
         [[tc GetTWebView] setURL:url];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"BLChangeURL" object:url userInfo:nil];
+    }else if([online_cache count] > OnlineCacheIndex){
+        NSString *word = [online_cache objectAtIndex:OnlineCacheIndex];
+        if(!word){
+            return NO;
+        }
+        NSString *URL = [NSString stringWithFormat:@"http://search.bilibili.com/all?keyword=%@",[word stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        WebTabView *tc = (WebTabView *)[browser activeTabContents];
+        [[tc GetTWebView] setURL:URL];
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"BLChangeURL" object:URL userInfo:nil];
     }else if(rowIndex == 4){
-        NSString *URL = [NSString stringWithFormat:@"http://search.bilibili.com/all?keyword=%@",sharedURLFieldString];
+        NSString *URL = [NSString stringWithFormat:@"http://search.bilibili.com/all?keyword=%@",[sharedURLFieldString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         WebTabView *tc = (WebTabView *)[browser activeTabContents];
         [[tc GetTWebView] setURL:URL];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"BLChangeURL" object:URL userInfo:nil];
     }else if(rowIndex == 5){
-        NSString *URL = [NSString stringWithFormat:@"https://www.google.com/search?q=%@",sharedURLFieldString];
+        NSString *URL = [NSString stringWithFormat:@"https://www.google.com/search?q=%@",[sharedURLFieldString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         WebTabView *tc = (WebTabView *)[browser activeTabContents];
         [[tc GetTWebView] setURL:URL];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"BLChangeURL" object:URL userInfo:nil];
@@ -59,8 +72,19 @@ extern NSString *sharedURLFieldString;
 
 - (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn
             row:(NSInteger)rowIndex{
-    if(rowIndex == 0){
+    BOOL isOLCacheOK = [sharedURLFieldString isEqualToString:online_cache_key];
+    if(rowIndex == 0 && !isOLCacheOK){
+        online_cache_key = [sharedURLFieldString copy];
+        NSLog(@"[AddressBar] Finding suggest data");
+        // Get 4 suggest from TST
         item_cache = [[PJTernarySearchTree sharedTree] retrievePrefix:sharedURLFieldString countLimit:4];
+        // If no result , add http://
+        if([item_cache count] == 0){
+            NSString *urlAddPrefix = [NSString stringWithFormat:@"http://%@",sharedURLFieldString];
+            item_cache = [[PJTernarySearchTree sharedTree] retrievePrefix:urlAddPrefix countLimit:4];
+        }
+        // Otherwise get from online
+        [self loadOnlineResultAsync:aTableView];
     }
     if([aTableColumn.identifier isEqualToString:@"st_icon_col"]){
         if(rowIndex < 4){
@@ -70,8 +94,12 @@ extern NSString *sharedURLFieldString;
         }
     }else if([aTableColumn.identifier isEqualToString:@"st_name_col"]){
         if(rowIndex < 4){
-            if([item_cache count] > rowIndex){
+            int LocalCacheCount = (int)[item_cache count];
+            int OnlineCacheIndex = (int)rowIndex - LocalCacheCount;
+            if(LocalCacheCount > rowIndex){
                 return [item_cache objectAtIndex:rowIndex];
+            }else if([online_cache count] > OnlineCacheIndex && isOLCacheOK){
+                return [online_cache objectAtIndex:OnlineCacheIndex];
             }else{
                 return @"No Result";
             }
@@ -84,6 +112,68 @@ extern NSString *sharedURLFieldString;
     }else{
         return @"ERROR";
     }
+}
+
+- (void)loadOnlineResultAsync:(NSTableView *)tv{
+    NSURLSessionConfiguration* sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:nil delegateQueue:nil];
+    
+    NSString * URLSTR = [NSString stringWithFormat:@"http://s.search.bilibili.com/main/suggest?func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&highlight=&userid=0&bangumi_acc_num=1&special_acc_num=0&topic_acc_num=0&upuser_acc_num=0&tag_num=10&special_num=0&bangumi_num=10&upuser_num=0&term=%@",[online_cache_key stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    NSString * currentKey = [online_cache_key copy];
+    NSURL* URL = [NSURL URLWithString:URLSTR];
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:URL];
+    request.HTTPMethod = @"GET";
+    
+    /* Start a new Task */
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            if([currentKey isEqualToString:online_cache_key]){
+                NSError *err;
+                NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+                if(err || !dic){
+                    return;
+                }
+                online_cache = [[NSMutableArray alloc] init];
+                // FXXK Objective-C JSON processing
+                if(dic[@"result"]){
+                    if(dic[@"result"][@"accurate"] && dic[@"result"][@"accurate"][@"bangumi"]){
+                        NSArray *b = dic[@"result"][@"accurate"][@"bangumi"];
+                        if([b count] > 0){
+                            for(int i = 0; i < [b count]; i++){
+                                NSString *bgmName = [b objectAtIndex:i][@"value"];
+                                if(bgmName){
+                                    [online_cache addObject:bgmName];
+                                }
+                            }
+                        }
+                    }
+                    if(dic[@"result"][@"tag"]){
+                        NSArray *tags = dic[@"result"][@"tag"];
+                        if([tags count] > 0){
+                            for(int i = 0; i < [tags count]; i++){
+                                NSString *tagName = [tags objectAtIndex:i][@"value"];
+                                if(tagName){
+                                    [online_cache addObject:tagName];
+                                }
+                            }
+                        }
+                    }
+                }
+                NSLog(@"[AddressBar] Online Suggest Load Succeeded with %lu object",(unsigned long)[online_cache count]);
+                dispatch_async(dispatch_get_main_queue(), ^(void){
+                    if(tv){
+                        [tv reloadData];
+                    }
+                });
+            }
+        }
+        else {
+            // Failure
+            NSLog(@"[AddressBar] Online Suggest Failed: %@", [error localizedDescription]);
+        }
+    }];
+    [task resume];
 }
 
 @end
