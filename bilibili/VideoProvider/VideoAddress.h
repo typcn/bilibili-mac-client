ee76cc2d1157ae6592191d5f3418ad8e9787648f
TYPCN <admin@typcn.com>
2016-03-04 01:55:30 +0800

    [VideoAddress] Add multi fragment support

--- bilibili/VideoProvider/VideoAddress.h
+++ bilibili/VideoProvider/VideoAddress.h
@@ -10,13 +10,18 @@
 
 @interface VideoAddress : NSObject
 
-@property NSString *firstFragmentURL;
-@property NSString *defaultPlayURL;
-@property NSArray *backupPlayURLs;
+@property NSString *firstFragmentURL; // First fragment of default play url
+@property NSMutableArray *defaultPlayURL; // All fragments [Frag1,Frag2,...]
+@property NSMutableArray *backupPlayURLs; // Backup URLs [ [B1F1,B1F2,...], [B2F1,B2F2,...], ... ]
 
 @property NSString *userAgent;
 @property NSString *cookie;
 
 - (NSString *)nextPlayURL;
+
+- (void)addDefaultPlayURL:(NSString *)URL;
+
+- (void)addBackupURL:(NSArray *)URL; // Multi fragments
+- (NSString *)processMultiFragment:(NSArray *)URLs; // Multi fragments
 
 @end
--- bilibili/VideoProvider/VideoAddress.m
+++ bilibili/VideoProvider/VideoAddress.m
@@ -12,20 +12,52 @@
     int currentURLIndex;
 }
 
+- (id)init{
+    self = [super init];
+    if(self){
+        self.defaultPlayURL = [[NSMutableArray alloc] init];
+        self.backupPlayURLs = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
 - (NSString *)nextPlayURL{
     if(!currentURLIndex){
         currentURLIndex = 1;
-        return self.defaultPlayURL;
-    }else if(self.backupPlayURLs){
+        return [self processMultiFragment:self.defaultPlayURL];
+    }else{
         int backupIdx = currentURLIndex - 1;
         int backupCount = (int)[self.backupPlayURLs count];
         if(backupCount > backupIdx){
-            return [self.backupPlayURLs objectAtIndex:backupIdx];
+            id urls = [self.backupPlayURLs objectAtIndex:backupIdx];
+            return [self processMultiFragment:urls];
         }else{
             return NULL;
         }
     }
-    return NULL;
+}
+
+- (void)addDefaultPlayURL:(NSString *)URL{
+    [self.defaultPlayURL addObject:URL];
+}
+
+- (void)addBackupURL:(NSArray *)URL{
+    [self.backupPlayURLs addObject:URL];
+}
+
+- (NSString *)processMultiFragment:(NSArray *)URLs{
+    if([URLs count] == 1){
+        return [URLs objectAtIndex:0];
+    }
+    NSString *edlUrl;
+    for (NSString *url in URLs) {
+        if(![edlUrl length]){
+            edlUrl = [NSString stringWithFormat:@"%@%@%lu%@%@%@", @"edl://", @"%",(unsigned long)[url length], @"%" , url ,@";"];
+        }else{
+            edlUrl = [NSString stringWithFormat:@"%@%@%lu%@%@%@",  edlUrl  , @"%",(unsigned long)[url length], @"%" , url ,@";"];
+        }
+    }
+    return edlUrl;
 }
 
 @end
