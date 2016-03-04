70af7621bf7d8e4585a139f32db8cfedc38d1e4c
TYPCN <admin@typcn.com>
2016-03-04 20:16:55 +0800

    [VideoProvider] Add you-get as new vp class

---
+++ bilibili/VideoProvider/VP_YouGet.h
@@ -0,0 +1,20 @@
+//
+//  VP_YouGet.h
+//  bilibili
+//
+//  Created by TYPCN on 2016/3/4.
+//  Copyright © 2016 TYPCN. All rights reserved.
+//
+
+#import "VideoProvider.h"
+
+#ifndef vp_youget_h
+#define vp_youget_h
+
+#define VP_YG_NOT_INSTALLED "You-Get Not Installed"
+
+#endif
+
+@interface VP_YouGet : VideoProvider
+
+@end
