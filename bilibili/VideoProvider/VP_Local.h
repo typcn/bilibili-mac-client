01b87163845efdce1daefdca02a2e7d01f934930
TYPCN <admin@typcn.com>
2016-03-06 08:54:49 +0800

    [VideoProvider] Add vp_local

---
+++ bilibili/VideoProvider/VP_Local.h
@@ -0,0 +1,13 @@
+//
+//  VP_Local.h
+//  bilibili
+//
+//  Created by TYPCN on 2016/3/6.
+//  Copyright © 2016 TYPCN. All rights reserved.
+//
+
+#import "VideoProvider.h"
+
+@interface VP_Local : VideoProvider
+
+@end
