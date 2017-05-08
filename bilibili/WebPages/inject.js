d06f217b2438ccba17e52be779a429a8f0d279b4
typcn <admin@typcn.com>
2017-05-08 16:15:53 +0800

    Bump to 2.52 ; MANUAL_PUBLISH

--- bilibili/WebPages/inject.js
+++ bilibili/WebPages/inject.js
@@ -1,4 +1,4 @@
-window.bilimacVersion = 251;
+window.bilimacVersion = 252;
 window.injectHTML = 'INJ_HTML';
 window.sendToView = function(data){
     var rpcdata = 'action=' + encodeURIComponent(data.action) + '&data=' + encodeURIComponent(data.data);
