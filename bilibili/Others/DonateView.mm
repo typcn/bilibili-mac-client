0024de22659dc0e15ec0160367809212a3cecc4b
typcn <admin@typcn.com>
2018-09-30 09:18:41 +0800

    Fix donate function

--- bilibili/Others/DonateView.mm
+++ bilibili/Others/DonateView.mm
@@ -10,8 +10,7 @@
 #import "Analytics.h"
 
 @interface DonateView ()
-@property (weak) IBOutlet NSTextField *PPdonateCount;
-@property (weak) IBOutlet NSTextField *alipayDonateCount;
+@property (weak) IBOutlet NSTextField *donateCount;
 
 @end
 
@@ -21,34 +20,12 @@
     [super viewDidLoad];
 }
 
-- (IBAction)PPdonateBtn:(id)sender {
-    NSString *base = @"https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=donate%40typcn%2ecom&lc=C2&item_name=Bilibili%20for%20mac%20%2d%20Donation&amount=";
-    NSString *after = @"%2e00&currency_code=USD&button_subtype=services&no_note=0&tax_rate=0%2e000&shipping=0%2e00&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest";
-    
-    int count = [self.PPdonateCount intValue];
-    
-    NSString *url = [NSString stringWithFormat:@"%@%d%@",base, count , after];
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:url]];
-    
-    [[NSUserDefaults standardUserDefaults] setObject:@"opened" forKey:@"donate"];
-    action("App","donate","paypal");
-}
-
 - (IBAction)alipayDonateBtn:(id)sender {
-    NSString *url = @"http://_bilimac_newtab.loli.video/donate.html";
+    NSString *url = [NSString stringWithFormat:@"https://typcn.com/pay?type=donate&amount=%d",[_donateCount intValue] ];
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:url]];
     
     [[NSUserDefaults standardUserDefaults] setObject:@"opened" forKey:@"donate"];
-    action("App","donate","alipay");
-}
-
-- (IBAction)copyBitcoinAddr:(id)sender {
-    NSPasteboard *pasteBoard = [NSPasteboard generalPasteboard];
-    [pasteBoard declareTypes:[NSArray arrayWithObjects:NSStringPboardType, nil] owner:nil];
-    [pasteBoard setString:@"1EprnUzBwcF52SZAnPZKRrj4rRaS6yhtQW" forType:NSStringPboardType];
-    
-    [[NSUserDefaults standardUserDefaults] setObject:@"copied" forKey:@"donate"];
-    action("App","donate","bitcoin");
+    action("App","donate","donate");
 }
 
 - (IBAction)hideBtn:(id)sender {
